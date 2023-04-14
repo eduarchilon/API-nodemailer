8abfb1987e17d4330ffb40873a3a839c728aaae9
eduarchilon <eduarchilon@outlook.es>
2023-04-14 17:41:37 -0300

    fix(nodemailer): fix email sender

--- src/config/emailer.js
+++ src/config/emailer.js
@@ -1,30 +1,33 @@
 const nodemailer = require('nodemailer')
 
-const createTrans = () => {
+const createTrans = (appemail, apppass) => {
   const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      // user: process.env.EMAIL_USER,
+      // pass: process.env.EMAIL_PASS,
+      user: appemail,
+      pass: apppass,
     },
   })
   return transport
 }
 
-const sendEmail = async (data) => {
-  const transporter = createTrans()
+const sendEmail = async (data, appemail, apppass) => {
+  const transporter = createTrans(appemail, apppass)
+  // const transporter = createTrans() with .env
   const { name, email, subject, html } = data
   const info = await transporter.sendMail({
-    from: `${name} ${email}`,
-    to: process.env.EMAIL_USER,
+    from: `${name + ' - ' + email} ${email}`,
+    to: appemail,
     subject: subject,
     html: html,
   })
 
   console.log('Message send: %s', info.messageId)
-  return;
+  return
 }
 
 module.exports = { sendEmail }
